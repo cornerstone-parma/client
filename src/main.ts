0199efc1f40f94df7bce0498d14211bee2232990
Mykyta <mp091689@gmail.com>
2024-07-09 13:57:37 -0400

    add axios, get data from api

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import axios from "axios";
+
+axios.defaults.baseURL = import.meta.env.VITE_REMOTE_API;
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
-
+app.use(createPinia())
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import {onMounted, ref} from "vue";
+import axios from "axios";
+
+const myVar = ref<String>("");
+
+onMounted(() => {
+  axios.get("/hello").then(response => {
+    console.log(response.data);
+    myVar.value = response.data;
+  });
+});
 </script>
 
 <template>
   <main>
+    <h1>{{myVar}}!</h1>
     <TheWelcome />
   </main>
 </template>
